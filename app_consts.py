a92ed4d5fbcb6b77a718022dca9bee1cd63c87ac
Alexandr Vorobey <nujniy.vorobey@gmail.com>
2023-12-18 00:06:12 +0300

    Adding string constants

--- app_consts.py
+++ app_consts.py
@@ -1,12 +1,23 @@
 from enum import StrEnum
 
 
+class AppPaths(StrEnum):
+    NEAT_CONFIG_STANDARD_PATH = "NeatConf.txt"
+
+
 class AppStrings(StrEnum):
     neat_breakout_title = "Breakout NEAT Algorithm"
 
     start_learning_button = "Начать обучение"
     load_generation_button = "Загрузить поколение"
     configure_settings_button = "Настроить параметры"
+
+    config_file_not_found = "Файл конфигурации не найден!"
+    set_path_to_config_file = "Укажите путь к файлу конфигурации."
+
+
+class NeatConfigStrings(StrEnum):
+    ...
 
 
 # TODO: Подумать над шрифтами
